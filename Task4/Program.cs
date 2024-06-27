5f696cce2c3970524d700d437675a9bfbcd92e8e
Sergey <okulov.sergej2016@yandex.ru>
2024-06-27 17:33:16 +0500

    Task 4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿string path1 = "C:\\Users\\ПК\\Desktop";
+string path2 = path1 + "/Students";
+Directory.CreateDirectory(path2);
+
+using (BinaryReader br = new BinaryReader(File.Open(path1 + "/students.dat", FileMode.Open)))
+{
+    while (br.PeekChar() > -1)
+    {
+        string Name = br.ReadString();
+        string Group = br.ReadString();
+        DateTime DateOfBirth = DateTime.FromBinary(br.ReadInt64());
+        decimal MiddleGrade = br.ReadDecimal();
+
+        string path3 = path2 + "/" + Group + ".txt";
+        FileInfo fileInfo = new FileInfo(path3);
+
+        if (!File.Exists(path3))
+        {
+            using (StreamWriter sw = fileInfo.CreateText())
+            {
+                sw.WriteLine(Name + " " + DateOfBirth + " " + MiddleGrade);
+            }
+        }
+        else
+        {
+            using (StreamWriter sw = fileInfo.AppendText())
+            {
+                sw.Write(Name + " " + DateOfBirth + " " + MiddleGrade);
+            }
+        }
+    }
+
+}
